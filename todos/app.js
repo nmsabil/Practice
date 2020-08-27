bcf8792c1bac5041b12a19e6e15d9d288a9c6239
Sabil <sabilmdtv1@gmail.com>
2020-08-27 17:39:49 +0100

    add item to ul function completed

---
+++ todos/app.js
@@ -0,0 +1,21 @@
+const addForm = document.querySelector(".add");
+const list = document.querySelector(".todos");
+
+const genetateTemplate = (todo) => {
+  const html = `
+  <li class="list-group-item d-flex justify-content-between align-item-center">
+     <span>${todo}</span>
+     <i class="fa fa-trash delete"></i>
+  </li>
+ `;
+  list.innerHTML += html;
+};
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = addForm.add.value.trim();
+  if (todo.length) {
+    genetateTemplate(todo);
+    addForm.reset();
+  }
+});
