3d98cb67878398e7999c85ed3704249a32f83c77
Sabil <sabilmdtv1@gmail.com>
2020-08-27 18:16:55 +0100

    filter function added

--- todos/app.js
+++ todos/app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 const genetateTemplate = (todo) => {
   const html = `
@@ -24,4 +25,19 @@ list.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.add("filtered"));
+
+  Array.from(list.children)
+    .filter((todo) => todo.textContent.toLowerCase().includes(term))
+    .forEach((todo) => todo.classList.remove("filtered"));
+};
+
+search.addEventListener("keyup", (e) => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
